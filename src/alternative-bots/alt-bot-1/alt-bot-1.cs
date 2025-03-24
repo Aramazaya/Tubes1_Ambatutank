4e7bbb565dc373cfc0d335b49b28c41291ffa7ff
Nayaka <19623116@mahasiswa.itb.ac.id>
2025-03-24 09:37:31 +0700

    feat: add functions for Artifical Potential Field Path Planning (TODO: actually implement it for the bot motion)

--- src/alternative-bots/alt-bot-1/alt-bot-1.cs
+++ src/alternative-bots/alt-bot-1/alt-bot-1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
@@ -10,6 +11,8 @@ using Robocode.TankRoyale.BotApi.Events;
 // ------------------------------------------------------------------
 public class NayakaBot : Bot
 {
+    bool foundEnemy = false;
+    List<TitikGaya> titikGaya = new List<TitikGaya>();
     static void Main(string[] args) {
         new NayakaBot().Start();
     }
@@ -31,26 +34,49 @@ public class NayakaBot : Bot
         AdjustRadarForBodyTurn = true;
         AdjustRadarForGunTurn = true;
         RadarTurnRate = 10;
-        TurnRadarRight(Double.PositiveInfinity);
         
         while (IsRunning) {
-            //if (RadarTurnRemaining == 0) {
-            //    SetTurnRadarRight(1);
-            //}
+            TurnRadarRight(360);
+
+            // ngejumlahin semua gaya yang diterima di list titikGaya
+            Vektor totalGaya = new Vektor(0, 0);
+            for (int i = 0; i < titikGaya.Count; i++) {
+                totalGaya += titikGaya[i].gaya;
+            }
+
+            // ngarahin bot ke arah totalGaya
+            //SetTurnRightRadians(totalGaya.getDirection() - HeadingRadians);
+
+            if (foundEnemy && Math.Abs(GunTurnRemaining) < 5) {
+                SetFire(1);
+                foundEnemy = false;
+            }
+            Go();
         }
     }
 
 
-    public override void OnScannedBot(ScannedBotEvent evt) {
-        Console.WriteLine(GunDirection);
-        Console.WriteLine(GunBearingTo(evt.X, evt.Y));
-
-        SetTurnGunLeft(GunBearingTo(evt.X, evt.Y));
-        
-        if (Math.Abs(TurnRemaining) < 10) {
-            Fire(0.3);
+    public override void OnScannedBot(ScannedBotEvent e) {
+        // cek apakah bot yang ketemu itu bot yang udah pernah ketemu atau belum
+        bool foundGaya = false;
+        for (int i = 0; i < titikGaya.Count; i++) {
+            if (titikGaya[i].id == e.ScannedBotId) {
+                foundGaya = true;
+         // kalo udah pernah ketemu, update gayanya
+                titikGaya[i].gaya = new Vektor(this.X, this.Y, this.Energy, e.X, e.Y, e.Energy);
+                break;
+            }
         }
-        //Fire(1);
+        // kalo belum pernah ketemu, tambahin ke list
+        if (!foundGaya) {
+            titikGaya.Add(new TitikGaya(this.X, this.Y, this.Energy, e.ScannedBotId, e.X, e.Y, e.Energy));
+        }
+
+
+        foundEnemy = true;
+        SetTurnGunLeft(GunBearingTo(e.X, e.Y));
+        //Console.WriteLine(GunBearingTo(evt.X, evt.Y));
+        //Console.WriteLine(Math.Atan((evt.X - this.X) / (evt.Y - this.Y)) * (180/Math.PI));
     }
 
 
@@ -70,14 +96,96 @@ public class NayakaBot : Bot
 
 
 // Point (x,y) class
-class Point
+class Vektor
 {
     public double X { get; set; }
     public double Y { get; set; }
 
-    public Point(double x, double y)
+    public Vektor(double length, double direction) {
+        // convert ke radian terus convert ke sistem koordinatnya Robocode (0 derajat tuh ngadep bawah)
+        direction *= Math.PI / 180;
+        direction -= Math.PI / 2;
+        // basic trigonometri
+        this.X = length * Math.Cos(direction);
+        this.Y = length * Math.Sin(direction);
+    }
+
+    public Vektor(double botX, double botY, double botEnergy, double musuhX, double musuhY, double musuhEnergy) {
+        double w1 = 20;
+        double w2 = 30;
+        double r = Math.Sqrt(Math.Pow(botX - musuhX, 2) + Math.Pow(botY - musuhY, 2));
+        double h = botEnergy / musuhEnergy;
+        double f = w1 / r + w2 * h;
+        double direction = Math.Atan2(musuhY - botY, musuhX - botX) * 180 / Math.PI;
+        direction *= Math.PI / 180;
+        direction -= Math.PI / 2;
+        this.X = f * Math.Cos(direction);
+        this.Y = f * Math.Sin(direction);
+    }
+
+    public static Vektor operator -(Vektor a)
     {
-        X = x;
-        Y = y;
+        return new Vektor(-a.X, -a.Y);
+    }
+
+    public static Vektor operator +(Vektor a, Vektor b)
+    {
+        return new Vektor(a.X + b.X, a.Y + b.Y);
+    }
+
+    public static Vektor operator -(Vektor a, Vektor b)
+    {
+        return new Vektor(a.X - b.X, a.Y - b.Y);
+    }
+
+    // ini buat dot product
+    public static double operator *(Vektor a, Vektor b)
+    {
+        return a.X * b.X + a.Y * b.Y;
+    }
+
+    public double getLength()
+    {
+        return Math.Sqrt(this.X * this.X + this.Y * this.Y);
+    }
+
+    public double getDirection()
+    {
+        // Adjusting the direction by rotating 90 degrees counterclockwise
+        return Math.Atan2(this.Y, this.X) + Math.PI / 2;
+    }
+}
+
+// ini bahasa jawanya musuh
+//class Mungsuh {
+//    public int id {  get; set; }
+//    public double energy { get; set; }
+//    public double direction { get; set; }
+//    public double speed { get; set; }
+
+//    public Mungsuh(int id, double energy, double direction, double speed) {
+//        this.id = id;
+//        this.energy = energy;
+//        this.direction = direction;
+//        this.speed = speed;
+//    }
+//}
+
+class TitikGaya {
+    public int id { get; set; }
+    public Vektor gaya { get; set; }
+    public TitikGaya(int id, Vektor gaya) {
+        this.id = id;
+        this.gaya = gaya;
+    }
+    
+    public TitikGaya(double botX, double botY, double botEnergy, int musuhID, double musuhX, double musuhY, double musuhEnergy) {
+        double w1 = 20;
+        double w2 = 30;
+        double r = Math.Sqrt(Math.Pow(botX - musuhX, 2) + Math.Pow(botY - musuhY, 2));
+        double h = musuhEnergy / botEnergy;
+        double f = w1 / r + w2 * h;
+        this.id = musuhID;
+        this.gaya = new Vektor(f, Math.Atan2(musuhY - botY, musuhX - botX) * 180 / Math.PI);
     }
 }
